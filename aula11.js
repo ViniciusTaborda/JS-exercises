ed651459c25df65ee78c214311d7723eec1af18c
ViniciusTaborda <vinieducosta+1@outlook.com>
2020-11-04 10:11:34 -0300

    Simple conditional concepts

---
+++ aula11.js
@@ -0,0 +1,10 @@
+//Condição simples
+
+var velocidade = 12;
+
+if (velocidade > 50){
+     console.log("multado!!")
+
+}else {
+     console.log("vai com deus")
+}
---
+++ aula11b.js
@@ -0,0 +1,9 @@
+// condição composta
+
+var pais = 'Brasil';
+
+if (pais == 'Brasil'){
+     console.log('Bom dia meu querido!')
+}else {
+     console.log('Good morning my friend!');
+}
